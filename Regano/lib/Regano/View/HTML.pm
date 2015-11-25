53497cbd5006bec926039736de58fa48aba30a6f
Pathore <pathore@8chan.co>
2015-11-24 20:56:14 -0600

    Add Template Toolkit View module

---
+++ Regano/lib/Regano/View/HTML.pm
@@ -0,0 +1,35 @@
+package Regano::View::HTML;
+use Moose;
+use namespace::autoclean;
+
+extends 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tt',
+    render_die => 1,
+);
+
+=head1 NAME
+
+Regano::View::HTML - TT View for Regano
+
+=head1 DESCRIPTION
+
+TT View for Regano HTML Pages.
+
+=head1 SEE ALSO
+
+L<Regano>
+
+=head1 AUTHOR
+
+Pathore
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ Regano/t/view_HTML.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+BEGIN { use_ok 'Regano::View::HTML' }
+
+done_testing();
