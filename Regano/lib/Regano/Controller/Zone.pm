c94a41b959af90ae53c42db8a6b52a2bf15d8bcd
Pathore <pathore@8chan.co>
2016-06-20 19:57:14 -0500

    Fix incorrect handling of zones that lack SOA records

--- Regano/lib/Regano/Controller/Zone.pm
+++ Regano/lib/Regano/Controller/Zone.pm
@@ -55,10 +55,11 @@ sub zone :Path :Args(1) {
 
     if ($type eq 'REGISTERED') {
 	# return records for single domain
+	my $records = $c->model('DB::Zone')->records_for_domain($zone_name);
+	shift @$records unless exists $records->[0];
 	$c->stash( template => 'zone/domain_'.$format.'.tt',
 		   zone => { name => $zone_name,
-			     records => $c->model('DB::Zone')
-				 ->records_for_domain($zone_name) } );
+			     records => $records } );
     } elsif ($type eq 'BAILIWICK') {
 	# return bailiwick zone
 	$c->response->body("Read $type zone $zone_name");
